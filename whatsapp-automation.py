7fba4c23d2af79d8ba85ee0c42d3f9d8db479e8a
Loredana Sandu <sanduloredanaa@gmail.com>
2020-11-22 16:36:31 +0100

    First version, without scheduling

---
+++ whatsapp-automation.py
@@ -0,0 +1,29 @@
+import time
+import json
+import os
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=chrome-data")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+
+chrome_browser = Chrome(executable_path= os.getcwd() + '/chromedriver_linux64/chromedriver', options=chrome_options)
+chrome_browser.get("https://web.whatsapp.com/")
+
+time.sleep(15)
+
+with open('./files/water.json', 'r') as data_file:
+    data = json.load(data_file)
+    name = data['name']
+    message = data['message']
+
+print("{}, {}".format(name, message))
+
+chrome_browser.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click()
+chrome_browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(message)
+chrome_browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span').click()
+
+time.sleep(10)
+chrome_browser.close()
