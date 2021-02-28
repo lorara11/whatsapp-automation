a478a72e3bd9ee6b4bde592e9113b35326a45be1
Loredana Sandu <sanduloredanaa@gmail.com>
2021-02-28 12:28:14 +0100

    Improved bash script for automation

--- whatsapp-automation.sh
+++ whatsapp-automation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-source /home/loredana/Stuff/Projects/whatsapp-automation/env/bin/activate;
-python3 /home/loredana/Stuff/Projects/whatsapp-automation/whatsapp-automation.py
+DISPLAY=:0
+PATH=/usr/local/bin/:/usr/bin:/usr/sbin
+
+while [ true ]; do
+	currenttime=$(date +%H:%M:%S)
+	if [[ "$(date +%A)" = "Sunday" ] || [ "$(date +%A)" = "Wednesday" ]] && [ "$currenttime" = "12:21:00" ]; then
+		source /home/loredana/Stuff/Projects/whatsapp-automation/env/bin/activate
+		python3 /home/loredana/Stuff/Projects/whatsapp-automation/whatsapp-automation.py
+	fi
+done
